84a081f29901fa87603af0c4a24f20c758d3feeb
elias f <eliasfaak@gmail.comgit statussdgit helloquitscdlsgitgit config --global user.email eliasfaak@gmail.com>
2021-01-21 18:50:36 +0100

    d

--- chessui.py
+++ chessui.py
@@ -1,3 +1,13 @@
 import pygame as pg
-from Chess import ChessEngine
 WIDTH = HEIGHT  = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT//DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+"""
+Metode for å ikke hente bilder flere ganger.
+"""
+def loadimages():
+    pieces = ["wP", "wR", "wN", "wB", "wQ", "wK", "bP", "bR", "bQ", "bB", "bN", "bK"]
+    IMAGES[piece] = p.transform.slace(p.image.load("brikker/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
