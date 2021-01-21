535f821cdf2352c18b42880d92d37a1e5a2af2a7
elias <eliasfaak@gmail.com>
2021-01-21 22:40:24 +0100

    chessboard image

--- chessmain.py
+++ chessmain.py
@@ -1,17 +1,61 @@
+import pygame as p
+import chessengine
+WIDTH = HEIGHT  = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT//DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
 """
-Denne klassen lagere all informasjonen om posisjonen og lovelige trekk
+Metode for å ikke hente bilder flere ganger.
 """
-class GameState():
-    def _init_(self):
-        self.board = [
-        ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"]
-        ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"]
-        ["--", "--", "--", "--", "--", "--", "--", "--"]
-        ["--", "--", "--", "--", "--", "--", "--", "--"]
-        ["--", "--", "--", "--", "--", "--", "--", "--"]
-        ["--", "--", "--", "--", "--", "--", "--", "--"]
-        ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"]
-        ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
-        ]
-    self.whiteToMove = True
-    self.moveLog
+def loadimages():
+    pieces = ["wP", "wR", "wN", "wB", "wQ", "wK", "bP", "bR", "bQ", "bB", "bN", "bK"]
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("/Users/eliasfakvam/Desktop/gitrepo/brikker/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+
+"""
+skal håndtere brukerinput og grafikk
+"""
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH,HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = chessengine.GameState()
+    loadimages()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+
+        clock.tick(MAX_FPS)
+        p.display.flip()
+        drawgamestate(screen, gs)
+
+"""
+grafikken
+"""
+def drawgamestate(screen, gs):
+    drawboard(screen) #lager bretter
+    drawpieces(screen, gs.board) #lager brikker
+
+
+def drawboard(screen):
+    colors =[p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[(r+c)%2]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def drawpieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+if __name__ == "__main__":
+    main()
